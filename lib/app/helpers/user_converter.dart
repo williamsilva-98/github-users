c527aed740f93aae8b0ecb1be37cc4285b5ab861
William Silva <williamhenrique.silva98@gmail.com>
2021-02-18 19:35:32 -0300

    Adição de verificação se a lista de usuários está nula, que caso positivo irá retornar null para o stream

--- lib/app/helpers/user_converter.dart
+++ lib/app/helpers/user_converter.dart
@@ -1,8 +1,10 @@
 import 'package:github_users/app/data/models/user_model.dart';
 
 class UserConverter {
-  static List<UserModel> convertUser(List users) {
+  static List<UserModel> convertUser(users) {
     List<UserModel> usersList = [];
+
+    if (users == null) return null;
 
     users.map((e) => {usersList.add(UserModel.fromJson(e))}).toList();
 
