b548d4db873e6165f0b1e36611fd265ae2d37757
William Silva <williamhenrique.silva98@gmail.com>
2021-02-18 17:42:05 -0300

    Refatoração do main widget

---
+++ lib/app_widget.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'app/pages/home/home_page.dart';
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
