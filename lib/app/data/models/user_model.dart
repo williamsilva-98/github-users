1945010206499bb7a3e370569ee70999af046052
William Silva <williamhenrique.silva98@gmail.com>
2021-02-19 22:29:23 -0300

    Remoção de atriutos desnecessários e adição do atributo responsável pela url do repositório do usuário

--- lib/app/data/models/user_model.dart
+++ lib/app/data/models/user_model.dart
@@ -2,25 +2,19 @@ class UserModel {
   int id;
   String login;
   String avatarUrl;
-  String url;
-  String followersUrl;
-  String reposUrl;
+  String htmlUrl;
 
   UserModel({
     this.id,
     this.login,
     this.avatarUrl,
-    this.url,
-    this.followersUrl,
-    this.reposUrl,
+    this.htmlUrl,
   });
 
   UserModel.fromJson(Map<String, dynamic> json) {
     id = json['id'];
     login = json['login'];
     avatarUrl = json['avatar_url'];
-    url = json['url'];
-    followersUrl = json['followers_url'];
-    reposUrl = json['repos_url'];
+    htmlUrl = json['html_url'];
   }
 }
