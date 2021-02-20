0ada53f2d8cef08429a74981af93a80a89d7cc2f
William Silva <williamhenrique.silva98@gmail.com>
2021-02-19 22:32:39 -0300

    Adição de um tema global

---
+++ lib/app/theme/app_theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppTheme {
+  static final ThemeData appTheme = ThemeData(
+    fontFamily: GoogleFonts.roboto().fontFamily,
+  );
+}
--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:github_users/app/pages/home/home_binding.dart';
+import 'package:github_users/app/theme/app_theme.dart';
 import 'app/pages/home/home_page.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
+      initialBinding: HomeBinding(),
+      theme: AppTheme.appTheme,
       home: HomePage(),
     );
   }
